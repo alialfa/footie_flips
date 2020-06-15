6a9d1b7aa8cb5ebcaca663a0480ddbee0f2957e0
alialfa <alphan.mongi@gmail.com>
2020-06-15 12:01:23 +0200

    modified dashboard.js + board .js & .css

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -3,7 +3,7 @@
 import React, { Component } from "react";
 import Gamecard from "../components/Gamecard";
 import ReactCardFlip from "react-card-flip"; // this is an ext module for animating card flips
-import { Row } from "react-bootstrap";
+import { Row, Container } from "react-bootstrap";
 
 //
 class Gameboard extends Component {
@@ -15,18 +15,6 @@ class Gameboard extends Component {
     // iterate via a map to prep render for complete game board of 16 cards
     // 8 front cards + 8 back cards
     const getGameCard = this.props.cardDeck.map((card, index) => {
-      /*
-        // React Card Flip Functionality
-        <FRONT_CCOMPONENT>
-        This is the front of the card.
-        <button onClick={this.handleClick}>Click to flip</button>
-        </YOUR_FRONT_CCOMPONENT>
-
-        <BACK_COMPONENT>
-        This is the back of the card.
-        <button onClick={this.handleClick}>Click to flip</button>
-        </YOUR_BACK_COMPONENT>
-        */
       return (
         <div>
           <ReactCardFlip isFlipped={card.isFlipped} flipDirection="vertical">
@@ -51,7 +39,9 @@ class Gameboard extends Component {
     // return full game board deck
     return (
       <div className="board">
-        <Row>{getGameCard}</Row>
+        <Container>
+          <Row md="4">{getGameCard}</Row>
+        </Container>
       </div>
     );
   }
