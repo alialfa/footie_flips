f83c7553e14685a9c1b6c27f32fcbc84a122d3d5
alialfa <alphan.mongi@gmail.com>
2020-06-15 19:46:44 +0200

    modified Game.js gamescore

--- src/components/Game.js
+++ src/components/Game.js
@@ -95,7 +95,7 @@ class Game extends Component {
       }
       // if 1st and 2nd card DONT match
       else {
-        this.setState({ gameScore: this.state.gameScore - 1 });
+        //this.setState({ gameScore: this.state.gameScore - 1 });
         this.flipTheCard(clickedCardID, true); // animate to reveal 2nd card
 
         // delay function is vital for user experience
